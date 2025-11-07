a4373b47f2221cdb13a20bd50fea5d1989bc4e9c
Mario Reder <mario.reder@pm.me>
2025-11-07 22:16:16 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Dreams are about to turn into reality",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Vaults,Rewards,Leaderboard,Markets",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Vaults,Rewards,Leaderboard,Markets",
   "VITE_CUSTOM_MENUS": "TWS Gurukul, https://twsgurukul.com",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
